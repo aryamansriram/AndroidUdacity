4c98b1cb88271c04d96b4863de9fa8703343a2e4
aryamansriram <aryaman.sriram@gmail.com>
2021-07-16 11:07:04 +0530

    Added sleep tracker app

--- sleep_tracker/app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerViewModel.kt
+++ sleep_tracker/app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerViewModel.kt
@@ -33,7 +33,7 @@ class SleepTrackerViewModel(
         private var viewModelJob = Job()
         private val uiScope = CoroutineScope(Dispatchers.Main +  viewModelJob)
         private var tonight = MutableLiveData<SleepNight?>()
-        private val nights = database.getAllNights()
+        val nights = database.getAllNights()
 
         private var _showSnackbarEvent = MutableLiveData<Boolean>()
 
